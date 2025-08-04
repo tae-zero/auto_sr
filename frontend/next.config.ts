a428e9fbc24adab2ed010c553fbea38f491e20d3
taeyeong-jeong0308 <jeongtaeteong0308@gmail.com>
2025-08-04 17:53:23 +0900

    수정40

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
   env: {
     PORT: process.env.PORT || '3000',
   },
