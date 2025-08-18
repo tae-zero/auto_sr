290d9cf31316c36c4c04423f4a27b0370217929c
checkuriboh <rma0126@gmail.com>
2025-08-18 20:09:55 +0900

    tnwjd2009

--- service/tcfdreport-service/app/main.py
+++ service/tcfdreport-service/app/main.py
@@ -91,5 +91,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("SERVICE_PORT", 8006))
+    port = int(os.getenv("SERVICE_PORT", 8004))
     uvicorn.run(app, host="0.0.0.0", port=port)
